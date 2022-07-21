518740182ca973dd1746bce6f30075f488183f12
Hema <hemasahu019@gmail.com>
2022-07-21 21:57:41 +0530

    Added P6 Compute Quotient and Remainder

---
+++ ComputeQuoNRem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramming
+{
+    public class ComputeQuoNRem
+    {
+        public void CompQnR()
+        {
+            int quotient, remainder; //declaring local variables
+
+            Console.Write("Please Enter the value for Dividend: ");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the value for Divisor: ");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            //Calculation
+            quotient = dividend / divisor;
+            remainder = dividend % divisor;
+
+            //Display results
+            Console.WriteLine("Quotient is : " + quotient);
+            Console.WriteLine("Remainder is : " + remainder);
+
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to Basic Core Programming");
             Console.WriteLine("Enter select an option to procced :");
-            Console.WriteLine("\n1. Flip Coin \n2. Check Leap year \n3. Nth Harmonic value \n4. Table of Power of 2 \n5. Find Prime Factors"); 
+            Console.WriteLine("\n1. Flip Coin \n2. Check Leap year \n3. Nth Harmonic value \n4. Table of Power of 2 \n5. Find Prime Factors \n6. Compute Quotient and Remainder "); 
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -30,6 +30,10 @@
                 case 5:
                     PrimeFactorization primeFactorization = new PrimeFactorization();
                     primeFactorization.FindPrimeFactorization();
+                    break;
+                case 6:
+                    ComputeQuoNRem computeQuoNRem = new ComputeQuoNRem();
+                    computeQuoNRem.CompQnR();
                     break;
                 default:
                     Console.WriteLine("Please enter a valid option");
