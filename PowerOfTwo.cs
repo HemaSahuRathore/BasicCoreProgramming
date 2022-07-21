2ede958a2aa9d6c456f99090bd7ef1efcfb7467e
Hema <hemasahu019@gmail.com>
2022-07-21 21:34:38 +0530

    [Added P4 Table of 2 to the Power N]

---
+++ PowerOfTwo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramming
+{
+    public class PowerOf2
+    {
+        public void TableOfTwo()
+        {   //Local Variables
+            int N;
+            int i = 1; //initialized to run the loop from 1 to N
+            int table = 1;
+
+            Console.Write("Enter the Value of N : "); //taking input from user
+            N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nTable of Power of 2");
+
+            while (i <= N)
+            {
+                table *= 2; //table = 2 * table
+                Console.WriteLine("2 to the Power {0} is {1}", i, table); //displays table of power of 2
+                i++;
+            }
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to Basic Core Programming");
             Console.WriteLine("Enter select an option to procced :");
-            Console.WriteLine("\n1. Flip Coin \n2. Check Leap year \n3. Nth Harmonic value"); 
+            Console.WriteLine("\n1. Flip Coin \n2. Check Leap year \n3. Nth Harmonic value \n4. Table of Power of 2 "); 
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -22,6 +22,10 @@
                 case 3:
                     HarmonicNumber harmonicNumber = new HarmonicNumber();
                     harmonicNumber.NthHarmonicValue();
+                    break;
+                case 4:
+                    PowerOf2 powerOf2 = new PowerOf2();
+                    powerOf2.TableOfTwo();
                     break;
                 default:
                     Console.WriteLine("Please enter a valid option");
